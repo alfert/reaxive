20706d820c5d4b4171e30290f9898c9a4db73cc3
Klaus Alfert <klaus.alfert@googlemail.com>
2014-04-17 20:43:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Reaxive
+
+Reaxive is a reactive event handling library, inspired by Elm (http://elm-lang.org).
+
+# Todo:
+[ ] Add tests
+[ ] Add liftn and foldp functions
+[ ] Add more descriptions
+
+
---
+++ mix.exs
@@ -0,0 +1,24 @@
+defmodule Reaxive.Mixfile do
+  use Mix.Project
+
+  def project do
+    [ app: :reaxive,
+      version: "0.0.1",
+      elixir: "~> 0.12.5",
+      deps: deps ]
+  end
+
+  # Configuration for the OTP application
+  def application do
+    [mod: { Reaxive, [] }]
+  end
+
+  # Returns the list of dependencies in the format:
+  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
+  #
+  # To specify particular versions, regardless of the tag, do:
+  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
+  defp deps do
+    []
+  end
+end
