868fa48eb95583308ce997e087b92205744b7d26
Klaus Alfert <klaus.alfert@googlemail.com>
2014-08-05 14:46:16 +0200

    Indentation with 4 spaces

--- lib/reaxive.ex
+++ lib/reaxive.ex
@@ -1,21 +1,21 @@
 defmodule Reaxive do
-  use Application
+    use Application
 
-# TODO:
-# put this part into a separate file
-# implement reactive streams as GenEvent Servers with lazy streams
-# and see how it works!
-#
-# GENERAL PROBLEM:
-# Streams are pull-type lazy enumerations, reactive extensions are push-type lazy enumerations.
-# How do the fit together? How do we implement the push-type enumerations?
+    # TODO:
+    # put this part into a separate file
+    # implement reactive streams as GenEvent Servers with lazy streams
+    # and see how it works!
+    #
+    # GENERAL PROBLEM:
+    # Streams are pull-type lazy enumerations, reactive extensions are push-type lazy enumerations.
+    # How do the fit together? How do we implement the push-type enumerations?
 
 
-  # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
-  # for more information on OTP Applications
-  def start(_type, _args) do
-    Reaxive.Supervisor.start_link
-  end
+    def start(_type, _args) do
+        Reaxive.Supervisor.start_link
+    end
 
-  
+
+
+      
 end
