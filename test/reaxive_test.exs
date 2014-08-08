57ef84c5802ee090dfa905536f4108f8d107df14
Klaus Alfert <klaus.alfert@googlemail.com>
2014-08-08 19:56:16 +0200

    Chaining test fails. Hmmm.

--- test/reaxive_test.exs
+++ test/reaxive_test.exs
@@ -17,7 +17,7 @@ defmodule ReaxiveTest do
 	test "send values to rx" do
 		value = :x
 		{:ok, rx} = Reaxive.Rx.Impl.start()
-		:ok = Reaxive.Rx.Impl.fun(rx, &(&1)) # identity fun
+		:ok = Reaxive.Rx.Impl.fun(rx, &identity/1) 
 		disp_me = Reaxive.Rx.Impl.subscribe(rx, simple_observer_fun(self))
 		Reaxive.Rx.Impl.on_next(rx, value)
 		assert_receive {:on_next, value}
@@ -30,7 +30,7 @@ defmodule ReaxiveTest do
 		Process.link(rx)
 		Process.flag(:trap_exit, true)
 
-		:ok = Reaxive.Rx.Impl.fun(rx, &(&1)) # identity fun
+		:ok = Reaxive.Rx.Impl.fun(rx, &identity/1) 
 		disp_me = Reaxive.Rx.Impl.subscribe(rx, simple_observer_fun(self))
 		Reaxive.Rx.Impl.on_completed(rx)
 		assert_receive {:on_completed, nil}
@@ -45,16 +45,38 @@ defmodule ReaxiveTest do
 		{:ok, rx} = Reaxive.Rx.Impl.start()
 		Process.link(rx)
 		Process.flag(:trap_exit, true)
-		:ok = Reaxive.Rx.Impl.fun(rx, fn(_) -> 1/0 end) # failing fun
+		:ok = Reaxive.Rx.Impl.fun(rx, fn(_) -> 1/0 end) # a failing fun
 		disp_me = Reaxive.Rx.Impl.subscribe(rx, simple_observer_fun(self))
 
 		Reaxive.Rx.Impl.on_next(rx, :x)
 		assert_receive {:on_error, msg}
 	end
 
+	test "chaining two Rx streams" do
+		{:ok, rx1} = Reaxive.Rx.Impl.start()
+		Process.link(rx1) #  just to ensure that failures appear also here!
+		:ok = Reaxive.Rx.Impl.fun(rx1, &identity/1) 
+
+		{:ok, rx2} = Reaxive.Rx.Impl.start()
+		Process.link(rx2) #  just to ensure that failures appear also here!
+		:ok = Reaxive.Rx.Impl.fun(rx2, &identity/1) 
+		src = Reaxive.Rx.Impl.subscribe(rx1, rx2)
+		:ok = Reaxive.Rx.Impl.source(rx2, src)
+
+		disp_me = Reaxive.Rx.Impl.subscribe(rx2, simple_observer_fun(self))
+
+		Reaxive.Rx.Impl.on_next(rx1, :x)
+		assert_receive {:on_next, :x}
+
+		Reaxive.Rx.Impl.on_completed(rx1)
+		assert_receive {:on_completed, nil}	
+	end
+
 	def simple_observer_fun(pid) do
 		fn(tag, value ) -> send(pid, {tag, value}) end
 	end
+
+	def identity(x), do: x
 
 	defimpl Observer, for: Function do
 		def on_next(observer, value), do: observer.(:on_next, value)
