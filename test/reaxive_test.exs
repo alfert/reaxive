55b66b46d378b97104de0823a1c3c3f06b4753c2
Klaus Alfert <klaus.alfert@googlemail.com>
2014-08-08 14:56:01 +0200

    First tests for Rx.Impl

--- test/reaxive_test.exs
+++ test/reaxive_test.exs
@@ -2,13 +2,39 @@ defmodule ReaxiveTest do
 	use ExUnit.Case
 
 	test "subscribe and dispose" do
-		{:ok, rx} = Reaxive.Rx.start()
-		disp_me = Reaxive.Rx.subscribe(rx, :me)
-		disp_you = Reaxive.Rx.subscribe(rx, :you)
+		{:ok, rx} = Reaxive.Rx.Impl.start()
+		disp_me = Reaxive.Rx.Impl.subscribe(rx, :me)
+		disp_you = Reaxive.Rx.Impl.subscribe(rx, :you)
 
-		assert Reaxive.Rx.subscribers(rx) == [:you, :me]
+		assert Reaxive.Rx.Impl.subscribers(rx) == [:you, :me]
 
 		Disposable.dispose(disp_me)
-		assert Reaxive.Rx.subscribers(rx) == [:you]
+		assert Reaxive.Rx.Impl.subscribers(rx) == [:you]
+		Disposable.dispose(disp_you)
+		assert Reaxive.Rx.Impl.subscribers(rx) == []
+	end
+
+	test "send values to rx" do
+		# how do we get some value from rx back?
+		value = :x
+		{:ok, rx} = Reaxive.Rx.Impl.start()
+		:ok = Reaxive.Rx.Impl.fun(rx, &(&1)) # identity fun
+		disp_me = Reaxive.Rx.Impl.subscribe(rx, simple_observer_fun(self))
+		Reaxive.Rx.Impl.on_next(rx, value)
+		assert_receive {:on_next, value}
+		Reaxive.Rx.Impl.on_completed(rx)
+		assert_receive {:on_completed, nil}
+		
+		catch_exit(Reaxive.Rx.Impl.on_next(rx, value))
+	end
+
+	def simple_observer_fun(pid) do
+		fn(tag, value ) -> send(pid, {tag, value}) end
+	end
+
+	defimpl Observer, for: Function do
+		def on_next(observer, value), do: observer.(:on_next, value)
+		def on_error(observer, exception), do: observer.(:on_error, exception)
+		def on_completed(observer), do: observer.(:on_completed, nil)
 	end
 end
