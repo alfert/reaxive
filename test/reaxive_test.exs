f74d1f71683aa66936f950da891b551db26b5439
Klaus Alfert <klaus.alfert@googlemail.com>
2014-08-09 14:00:25 +0200

    Chaining works, failures are propagated. But subscriptions are not cleared

--- test/reaxive_test.exs
+++ test/reaxive_test.exs
@@ -95,8 +95,37 @@ defmodule ReaxiveTest do
 		assert_receive {:on_completed, nil}
 
 		assert Reaxive.Rx.Impl.subscribers(rx1) == []
-		assert Reaxive.Rx.Impl.subscribers(rx2) == []
-	
+		assert Reaxive.Rx.Impl.subscribers(rx2) == []	
+	end
+
+	test "chaining two Rx streams with failures" do
+		Process.flag(:trap_exit, true)
+		{:ok, rx1} = Reaxive.Rx.Impl.start()
+		Process.link(rx1) #  just to ensure that failures appear also here!
+		:ok = Reaxive.Rx.Impl.fun(rx1, &identity/1) 
+
+		{:ok, rx2} = Reaxive.Rx.Impl.start()
+		Process.link(rx2) #  just to ensure that failures appear also here!
+		:ok = Reaxive.Rx.Impl.fun(rx2, fn(x) -> 
+			IO.puts "Yeah #{inspect x}"
+			1/0 end) # will always fail 
+		src = Reaxive.Rx.Impl.subscribe(rx1, rx2)
+		:ok = Reaxive.Rx.Impl.source(rx2, src)
+
+		call_me = simple_observer_fun(self)
+		disp_me = Reaxive.Rx.Impl.subscribe(rx2, call_me)
+
+		assert Reaxive.Rx.Impl.subscribers(rx1) == [rx2]
+		assert Reaxive.Rx.Impl.subscribers(rx2) == [call_me]
+
+		Reaxive.Rx.Impl.on_next(rx1, :x)
+		assert_receive {:on_error, _}
+
+		Reaxive.Rx.Impl.on_completed(rx1)
+		assert_receive {:on_completed, nil}
+
+		assert Reaxive.Rx.Impl.subscribers(rx1) == []
+		assert Reaxive.Rx.Impl.subscribers(rx2) == []	
 	end
 
 	def simple_observer_fun(pid) do
@@ -104,6 +133,20 @@ defmodule ReaxiveTest do
 	end
 
 	def identity(x), do: x
+
+	def drain_messages(wait_millis \\ 50)
+	def drain_messages(wait_millis) when is_integer(wait_millis) do
+		receive do
+		after wait_millis -> :ok
+		end
+		drain_messages([])
+	end
+	def drain_messages(accu) when is_list(accu) do
+		receive do
+			msg     -> drain_messages([msg | accu])
+			after 0 -> Enum.reverse(accu)
+		end
+	end
 
 	defimpl Observer, for: Function do
 		def on_next(observer, value), do: observer.(:on_next, value)
