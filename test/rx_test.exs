5261e20e9d3807e325be7714097481df7e7c9345
Klaus Alfert <klaus.alfert@googlemail.com>
2014-08-11 17:26:04 +0200

    Using |> with map works properly

--- test/rx_test.exs
+++ test/rx_test.exs
@@ -14,13 +14,36 @@ defmodule RxTest do
 		assert Reaxive.Rx.Impl.subscribers(rx) == [rx1]
 		assert Reaxive.Rx.Impl.subscribers(rx1) == [o]
 
-		# We need to implement the same chain from hand in rx_impl_test to ensure 
-		# that our infrastructure works as expected.
-
 		Reaxive.Rx.Impl.on_next(rx, 1)
 		assert_receive {:on_next, 2}
 		Reaxive.Rx.Impl.on_completed(rx)
 		assert_receive {:on_completed, nil}
+		Disposable.dispose(rx2)
+		refute Process.alive?(rx)
+	end
+
+	test "map several values via |>" do
+		{:ok, rx} = Reaxive.Rx.Impl.start()
+		:ok = Reaxive.Rx.Impl.fun(rx, &identity/1) 
+		o = simple_observer_fun(self)
+
+		rx2 = rx |> Reaxive.Rx.map(&(&1 + 1)) |> Observable.subscribe(o)
+
+		Reaxive.Rx.Impl.on_next(rx, 1)
+		assert_receive {:on_next, 2}
+
+		Observer.on_next(rx, 2)
+		assert_receive {:on_next, 3}
+
+		values = [1, 2, 3, 4]
+		values |> Enum.each fn(v) -> 
+			Observer.on_next(rx, v) 
+			k = v+1
+			assert_receive {:on_next, ^k} 
+		end
+		Observer.on_completed(rx)
+		assert_receive {:on_completed, nil}
+		
 		Disposable.dispose(rx2)
 		refute Process.alive?(rx)
 	end
