7203dcb09e4ef3f357a6059df053c6117e0c6e98
Klaus Alfert <klaus.alfert@googlemail.com>
2014-08-13 16:19:14 +0200

    Shorter formulation via require Reaxive.Rx, as: Rx

--- test/rx_test.exs
+++ test/rx_test.exs
@@ -2,33 +2,34 @@ defmodule RxTest do
 	use ExUnit.Case
 	import ReaxiveTestTools
 	require Integer
+	require Reaxive.Rx, as: Rx
 
 	test "map function works" do
 		value = 1
-		{:ok, rx} = Reaxive.Rx.Impl.start()
+		{:ok, rx} = Rx.Impl.start()
 		
-		rx1 = rx |> Reaxive.Rx.map &(&1 + 1) 
+		rx1 = rx |> Rx.map &(&1 + 1) 
 		o = simple_observer_fun(self)
 		rx2 = Observable.subscribe(rx1, o)
 
-		assert Reaxive.Rx.Impl.subscribers(rx) == [rx1]
-		assert Reaxive.Rx.Impl.subscribers(rx1) == [o]
+		assert Rx.Impl.subscribers(rx) == [rx1]
+		assert Rx.Impl.subscribers(rx1) == [o]
 
-		Reaxive.Rx.Impl.on_next(rx, 1)
+		Rx.Impl.on_next(rx, 1)
 		assert_receive {:on_next, 2}
-		Reaxive.Rx.Impl.on_completed(rx)
+		Rx.Impl.on_completed(rx)
 		assert_receive {:on_completed, nil}
 		Disposable.dispose(rx2)
 		refute Process.alive?(rx)
 	end
 
 	test "map several values via |>" do
-		{:ok, rx} = Reaxive.Rx.Impl.start()
+		{:ok, rx} = Rx.Impl.start()
 		o = simple_observer_fun(self)
 
-		rx2 = rx |> Reaxive.Rx.map(&(&1 + 1)) |> Observable.subscribe(o)
+		rx2 = rx |> Rx.map(&(&1 + 1)) |> Observable.subscribe(o)
 
-		Reaxive.Rx.Impl.on_next(rx, 1)
+		Rx.Impl.on_next(rx, 1)
 		assert_receive {:on_next, 2}
 
 		Observer.on_next(rx, 2)
@@ -50,7 +51,7 @@ defmodule RxTest do
 	test "generate some values" do
 		values = [1, 2, 3, 4]
 		o = simple_observer_fun(self)
-		values |> Reaxive.Rx.generate |> Observable.subscribe(o)
+		values |> Rx.generate |> Observable.subscribe(o)
 		
 		values |> Enum.each fn(v) ->
 			assert_receive{:on_next, ^v} end
@@ -59,29 +60,29 @@ defmodule RxTest do
 
 	test "print out generated values" do
 		values = [1, 2, 3, 4]
-		values |> Reaxive.Rx.generate |> Reaxive.Rx.as_text
+		values |> Rx.generate |> Rx.as_text
 	end
 
 	test "create a stream from a sequence of events" do
 		values = 1..20 |> Enum.to_list
-		l = values |> Reaxive.Rx.generate(1) |> 
-			Reaxive.Rx.as_text |> Reaxive.Rx.stream |> Enum.to_list
+		l = values |> Rx.generate(1) |> 
+			Rx.as_text |> Rx.stream |> Enum.to_list
 		# l = s |> Enum.to_list
 		assert values == l
 	end
 
 	test "abort a sequence early on via generate and stream" do
 		all = 1..1000 |> Enum.to_list
-		five = all |> Reaxive.Rx.generate(1) |>
-			Reaxive.Rx.as_text |> Reaxive.Rx.stream |> Stream.take(5) |> Enum.to_list
+		five = all |> Rx.generate(1) |>
+			Rx.as_text |> Rx.stream |> Stream.take(5) |> Enum.to_list
 
 		assert five == (all |> Enum.take(5))
 	end
 
 	test "filter out all odd numbers" do
 		values = 1..20 |> Enum.to_list
-		odds = values |> Reaxive.Rx.generate(1) |> Reaxive.Rx.filter(&Integer.odd?/1) |>
-			 Reaxive.Rx.stream |> Enum.to_list
+		odds = values |> Rx.generate(1) |> Rx.filter(&Integer.odd?/1) |>
+			 Rx.stream |> Enum.to_list
 
 		assert Enum.all?(odds, &Integer.odd?/1)
 		assert odds == (values |> Enum.filter(&Integer.odd?/1))
