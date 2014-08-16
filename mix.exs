dc84b9b71304ebe26073d16c108a03450916e8cc
Klaus Alfert <klaus.alfert@googlemail.com>
2014-08-16 16:01:49 +0200

    Deleted old comment on deps

--- mix.exs
+++ mix.exs
@@ -19,7 +19,6 @@ defmodule Reaxive.Mixfile do
 
   defp deps do
     [      
-      # Generate documentation with ex_doc, valid for Elixir 0.14.1
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.5", only: :dev}
    ]
