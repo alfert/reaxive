4e49e274ea853f85c9961e951a996082250cfd29
Klaus Alfert <klaus.alfert@googlemail.com>
2014-08-20 22:19:15 +0200

    Added coverex and dialyze

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Reaxive.Mixfile do
       version: "0.0.1-dev",
       elixir: "~> 0.15",
       docs: [readme: true],
+      test_coverage: [tool: Coverex.Task, log: :info],
       deps: deps ]
   end
 
@@ -19,8 +20,11 @@ defmodule Reaxive.Mixfile do
 
   defp deps do
     [      
+      {:coverex, "~> 0.0.7", only: :test},
+      # {:coverex, "~> 0.0.7-dev", path: "../coverex", only: :test},
       {:earmark, "~> 0.1", only: :dev},
-      {:ex_doc, "~> 0.5", only: :dev}
+      {:ex_doc, "~> 0.5", only: :dev},
+      {:dialyze, "~> 0.1.2", only: :dev}
    ]
   end
 end
