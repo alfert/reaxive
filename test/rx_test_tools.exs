790cc32e2a7c1d5723a7dc9aa9b4a3bda76200af
Klaus Alfert <klaus.alfert@googlemail.com>
2014-08-30 16:32:38 +0200

    Simple_observer logs its messages

--- test/rx_test_tools.exs
+++ test/rx_test_tools.exs
@@ -1,7 +1,12 @@
 defmodule ReaxiveTestTools do
 		
+	require Logger
+
 	def simple_observer_fun(pid) do
-		fn(tag, value ) -> send(pid, {tag, value}) end
+		fn(tag, value ) -> 
+			Logger.debug "simple_observer: #{inspect {tag, value}}"
+			send(pid, {tag, value}) 
+		end
 	end
 
 	def identity(x), do: x
