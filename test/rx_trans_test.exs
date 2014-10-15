be8664f2fb31572e34eda192b13631cd0091c310
Klaus Alfert <klaus.alfert@googlemail.com>
2014-10-16 01:06:14 +0200

    Nested transducer is slower ??!!

--- test/rx_trans_test.exs
+++ test/rx_trans_test.exs
@@ -28,4 +28,32 @@ defmodule RxTransduceTest do
 		assert m == Enum.take_while(l, p)
 	end
 
+	test "map and filter" do
+		l = 1..20 |> Enum.to_list
+
+		# f = Trans.filtering(&Integer.is_odd/1)
+		# m = Trans.mapping(&inc/1) 
+
+		mf = Trans.mapped_filtering(&inc/1, &Integer.is_odd/1)
+		m = Trans.transduce(l, mf) |> Enum.reverse
+
+		assert m == l |> Enum.map(&inc/1) |> Enum.filter(&Integer.is_odd/1)
+	end
+
+	test "performance test transduce" do
+		l = 1..1_000_000 |> Enum.to_list
+		t = fn() -> l |> Enum.map(&inc/1) |> Enum.filter(&Integer.is_odd/1) end
+		m = fn() -> 
+			mf = Trans.mapped_filtering(&inc/1, &Integer.is_odd/1)
+			Trans.transduce(l, mf) |> Enum.reverse
+		end
+
+		{dur_trans, v2} = :timer.tc(m)
+		{dur_enum, v1} = :timer.tc(t)
+		
+		assert v1 == v2
+		assert dur_trans < dur_enum
+		assert dur_trans * 1.5 < dur_enum
+	end
+
 end
