3c5128730620adb5dae48add64c5b5d116b8f0f8
Klaus Alfert <klaus.alfert@googlemail.com>
2014-11-08 19:23:35 +0100

    Re-enable coverex, reordering the applications to distinguish between test and regular apps

--- mix.exs
+++ mix.exs
@@ -6,10 +6,6 @@ defmodule Reaxive.Mixfile do
       version: "0.0.2-dev",
       elixir: "~> 1.0.0",
       docs: [readme: true],
-
-      #########
-      ## Something on coveralls fails - why???
-      ##   what is different to coverex?
       test_coverage: [tool: Coverex.Task, log: :info, coveralls: true],
       deps: deps ]
   end
@@ -18,14 +14,17 @@ defmodule Reaxive.Mixfile do
   def application do
     [
       mod: { Reaxive, [] },
-      applications: [:kernel , :stdlib, :sasl, :logger, :httpoison]
+      applications: [
+        # httpoison is used for coverage testing only ...
+        :httpoison, 
+        # regular dependencies
+        :kernel , :stdlib, :sasl, :logger]
     ]
   end
 
   defp deps do
     [      
       {:coverex, "~> 1.0.0", only: :test},
-      # {:coverex, "~> 0.0.7-dev", path: "../coverex", only: :test},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.6", only: :dev},
       {:dialyze, "~> 0.1.2", only: :dev},
