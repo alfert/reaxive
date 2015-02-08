3290c7c548d24b11d895bf9a61ccf45ebcb0f08a
Klaus Alfert <klaus.alfert@googlemail.com>
2015-02-08 18:25:29 +0100

    Prepare for next release

--- release.sh
+++ release.sh
@@ -3,8 +3,8 @@
 set -x
 
 # old is always with -dev
-old="0.0.1"
-new="0.0.2-dev"
+old="0.0.2"
+new="0.0.3-dev"
 # do not set any variables beyond this line
 old_version="$old-dev"
 release_version="$old"
@@ -31,6 +31,7 @@ git tag -a -m "new release version v$release_version" v$release_version
 
 # Upload to Hex.PM
 mix hex.publish
+mix hex.docs
 
 # update version in mix.exs
 sed -i "s/\(version: \"\)$release_version\",/\\1$new_version\",/" mix.exs
