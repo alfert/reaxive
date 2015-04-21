be5203833232701ae023ca7f359b8d9c0b6971a9
Klaus Alfert <klaus.alfert@googlemail.com>
2015-04-21 22:37:28 +0200

    An emtpy subscription for testing

--- test/rx_test_tools.exs
+++ test/rx_test_tools.exs
@@ -15,4 +15,19 @@ defmodule ReaxiveTestTools do
 	def double(x), do: x+x
 	def p(x), do: IO.inspect x
 
+	defmodule EmptySubscription do
+		@moduledoc """
+		This subscription does nothing. It is always unsubscribed 
+		and requires no running process. 
+		"""
+		defstruct pid: self()
+
+		def new, do: %__MODULE__{}
+
+		defimpl Subscription do
+			def unsubscribe(sub), do: :ok
+			def is_unsubscribed?(sub), do: true
+		end
+	end
+
 end	
