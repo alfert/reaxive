afefae0d67cd2271d9d745ee7d1b3355fc4b0ef2
Klaus Alfert <klaus.alfert@googlemail.com>
2015-09-08 14:58:09 +0200

    Allow Elixir 1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Reaxive.Mixfile do
   def project do
     [ app: :reaxive,
       version: "0.1.0-dev",
-      elixir: "~> 1.0.0",
+      elixir: "~> 1.0",
       package: package,
       name: "Reaxive - a Reactive Extension inspired library for Elixir",
       description: description,
