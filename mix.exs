aeab8c056ae02918a63a2af311ea5a7fcb87b0cc
Klaus Alfert <klaus.alfert@gmail.com>
2015-10-10 13:09:57 +0200

    bump version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Reaxive.Mixfile do
 
   def project do
     [ app: :reaxive,
-      version: "0.1.0-dev",
+      version: "0.1.0",
       elixir: "~> 1.0",
       package: package,
       name: "Reaxive - a Reactive Extension inspired library for Elixir",
